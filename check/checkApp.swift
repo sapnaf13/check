c58686e0ad972365b4b84692b4df8ed90b026a4a
Sapna Fulwani <imsapnaf13@gmail.com>
2024-06-05 11:23:45 +0530

    Initial Commit

---
+++ check/checkApp.swift
@@ -0,0 +1,17 @@
+//
+//  checkApp.swift
+//  check
+//
+//  Created by Harish Fulwani on 05/06/24.
+//
+
+import SwiftUI
+
+@main
+struct checkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
